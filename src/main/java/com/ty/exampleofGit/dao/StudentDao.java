0a9311f3457634fdde632cea9717b7f2dd6c1b0c
Manu <manojy0505@gmail.com>
2023-02-02 15:20:19 +0530

    Student Dao Created

---
+++ src/main/java/com/ty/exampleofGit/dao/StudentDao.java
@@ -0,0 +1,11 @@
+package com.ty.exampleofGit.dao;
+
+import com.ty.exampleofGit.dto.Student;
+
+public class StudentDao {
+	
+	public Student saveStudent(Student student) {
+		//Implement to save Student detalies in DB
+		return student;
+	}
+}
